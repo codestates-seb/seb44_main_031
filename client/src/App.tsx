c9e0e58b910fac03783e9e485ef00b26ea3af556
Jeon_Ye_Hun <yahun722@naver.com>
2023-07-06 15:32:37 +0900

    ✨feat:산책 상세페이지 레이아웃 구현 완료(2023.07.06 전예훈)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import GlobalStyle from './components/styles/GlobalStyled';
-
+import WalkMateDetail from './features/walk-Mate-Detail/WalkMateDetail';
 function App() {
   return (
     <div>
@@ -8,8 +8,8 @@ function App() {
       <Routes>
         <Route
           path="/"
-          // element={<p>초기구현 테스트 완료 - 나중에 지워주세요</p>}
-          element={<p>초기화면 구현 테스트</p>}
+          //
+          element={<WalkMateDetail />}
         />
       </Routes>
     </div>
