bc135376756f70fc64df1d7c49c3460ad4d4fe49
youngtakleeme <20takki@gmail.com>
2023-07-07 15:27:49 +0900

    💄 design: 공용 버튼 추가 (2023.07.07 이영탁)

--- client/src/features/walk-mate-create/WalkMateCreate.tsx
+++ client/src/features/walk-mate-create/WalkMateCreate.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { StyledButtonPink3D } from '../../components/styles/StyledButtons';
 import { styled } from 'styled-components';
 
 declare global {
@@ -108,7 +109,7 @@ const WalkMateCreate = () => {
           <p className="error-message">error message</p>
         </div>
         <div className="btn-submit-container">
-          <StyledPinkButton3D type="button" role="button">
+          <StyledButtonPink3D type="button" role="button">
             <span className="span-img-container">
               <span>산책 모임 개설하기</span>
               <img
@@ -118,7 +119,7 @@ const WalkMateCreate = () => {
                 height={25}
               />
             </span>
-          </StyledPinkButton3D>
+          </StyledButtonPink3D>
         </div>
       </StyledForm>
     </FormDivContainer>
@@ -215,52 +216,3 @@ const StyledForm = styled.form`
     height: 50px;
   }
 `;
-
-const StyledPinkButton3D = styled.button`
-  background-color: var(--pink-400);
-  border: 2px solid var(--pink-400);
-  border-radius: 30px;
-  box-shadow: var(--pink-100) 4px 4px 0 0;
-  color: white;
-  cursor: pointer;
-  display: inline-block;
-  font-weight: 600;
-  font-size: 16px;
-  padding: 0 18px;
-  line-height: 40px;
-  text-align: center;
-  text-decoration: none;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-
-  &:hover {
-    background-color: rgb(255, 84, 141);
-    border: 2px solid rgb(255, 84, 141);
-  }
-
-  &:active {
-    box-shadow: var(--pink-300) 2px 2px 0 0;
-    transform: translate(2px, 2px);
-  }
-
-  @media (min-width: 768px) {
-    & {
-      min-width: 120px;
-      padding: 0 25px;
-    }
-  }
-`;
-
-// const StyeldButton = styled.button`
-//   font-weight: bold;
-//   padding: 5px;
-//   color: white;
-//   background-color: var(--pink-400);
-//   height: 40px;
-//   width: 100px;
-//   cursor: pointer;
-
-//   border: none;
-//   border-radius: 15px;
-// `;
