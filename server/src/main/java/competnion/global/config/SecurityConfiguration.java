52829e0ff05fe791f247ebab4684f35584162659
jeongmin0218 <dlawjdals0218@gmail.com>
2023-07-14 14:19:59 +0900

    no message

--- server/src/main/java/competnion/global/config/SecurityConfiguration.java
+++ server/src/main/java/competnion/global/config/SecurityConfiguration.java
@@ -133,7 +133,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new JwtParseInterceptor(jwtUtils()))
-                .addPathPatterns(List.of("/articles/**"));
+                .addPathPatterns(List.of("/articles/**", "/auth/**"));
     }
 
     @Override
