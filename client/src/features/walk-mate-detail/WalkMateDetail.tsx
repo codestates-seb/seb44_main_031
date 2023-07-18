3b3a3bb9e83767a85499f7cca6dd1df85a88adc0
Jeon_Ye_Hun <yahun722@naver.com>
2023-07-18 13:50:42 +0900

    conflict 에러 해결중

--- client/src/features/walk-mate-detail/WalkMateDetail.tsx
+++ client/src/features/walk-mate-detail/WalkMateDetail.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import WalkMateDetailHeader from './WalkMateDetailHeader';
 import WalkMateDetailBody from './WalkMateDetailBody';
 import { styled } from 'styled-components';
