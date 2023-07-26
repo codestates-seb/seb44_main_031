7d7a4dabb9364bcd8e7d14ca43d20f21979086d2
jeongmin0218 <dlawjdals0218@gmail.com>
2023-07-26 11:12:20 +0900

    no message

--- server/src/main/java/competnion/domain/auth/service/AuthService.java
+++ server/src/main/java/competnion/domain/auth/service/AuthService.java
@@ -199,7 +199,7 @@ public class AuthService {
                 .point(point)
                 .latitude(request.getLatitude())
                 .longitude(request.getLongitude())
-                .imgUrl("https://mybucketforpetmily.s3.ap-northeast-2.amazonaws.com/dog.png")
+                .imgUrl("https://mybucketforpetmily.s3.ap-northeast-2.amazonaws.com/image/dog.png")
                 .roles(roles)
                 .build());
     }
