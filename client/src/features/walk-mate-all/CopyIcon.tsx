9a18f1e0c6bd660c5b68666d7208f69764edf654
박지훈 <jihoon@bagjihun-ui-MacBookPro.local>
2023-07-26 18:28:00 +0900

    마지막 작업전 Pull

--- client/src/features/walk-mate-all/CopyIcon.tsx
+++ client/src/features/walk-mate-all/CopyIcon.tsx
@@ -4,34 +4,6 @@ import { toast } from 'react-toastify';
 import { styled } from 'styled-components';
 import ClipboardJS from 'clipboard';
 
-// const CopyIcon = ({ textToCopy }: { textToCopy: string }) => {
-//   const handleCopyClick = async (event: React.MouseEvent<HTMLElement>) => {
-//     event.stopPropagation();
-//     try {
-//       await navigator.clipboard.writeText(textToCopy);
-//       toast.success('해당 모임 URL이 복사되었습니다!', {
-//         toastId: 'copy-success',
-//         autoClose: 900,
-//         position: 'bottom-center',
-//         hideProgressBar: true,
-//       });
-//     } catch (error) {
-//       toast.error(`Failed to copy URL: ${error}`, {
-//         toastId: 'copy-error',
-//         autoClose: 900,
-//         position: 'bottom-center',
-//       });
-//     }
-//   };
-
-//   return (
-//     <StyledCopyIconContainer>
-//       <IoCopyOutline className="copy-icon" onClick={handleCopyClick} />
-//     </StyledCopyIconContainer>
-//   );
-// };
-
-// 라이브러리
 const CopyIcon = ({ textToCopy }: { textToCopy: string }) => {
   const handleCopyClick = (event: React.MouseEvent<HTMLElement>) => {
     event.stopPropagation();
@@ -67,41 +39,7 @@ const CopyIcon = ({ textToCopy }: { textToCopy: string }) => {
   );
 };
 
-// 기능은 안되고 에러메세지 띄워주기
-// const CopyIcon = ({ textToCopy }: { textToCopy: string }) => {
-//   const handleCopyClick = async (event: React.MouseEvent<HTMLElement>) => {
-//     event.stopPropagation();
-//     try {
-//       if (navigator.clipboard) {
-//         await navigator.clipboard.writeText(textToCopy);
-//         toast.success('해당 모임 URL이 복사되었습니다!', {
-//           toastId: 'copy-success',
-//           autoClose: 900,
-//           position: 'bottom-center',
-//           hideProgressBar: true,
-//         });
-//       } else {
-//         toast.error('해당 브라우저에서는 복사 기능을 지원하지 않습니다.', {
-//           toastId: 'copy-error',
-//           autoClose: 900,
-//           position: 'bottom-center',
-//         });
-//       }
-//     } catch (error) {
-//       toast.error(`Failed to copy URL: ${error}`, {
-//         toastId: 'copy-error',
-//         autoClose: 900,
-//         position: 'bottom-center',
-//       });
-//     }
-//   };
 
-//   return (
-//     <StyledCopyIconContainer>
-//       <IoCopyOutline className="copy-icon" onClick={handleCopyClick} />
-//     </StyledCopyIconContainer>
-//   );
-// };
 
 const StyledCopyIconContainer = styled.div`
   .copy-icon {
